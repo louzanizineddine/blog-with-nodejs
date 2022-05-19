a629483422811cb87468e86fc11486422f806a34
louzanizineddine <louzanizineddine@gmail.com>
2022-05-19 23:14:27 +0100

    created the form to add articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,29 @@
 const express = require('express');
-
+const Article = require('./../modals/article')
+const {forward} = require("console-control-strings");
 const router = express.Router();
 
 router.get('/new' , (req , res) => {
     res.render('articles/new')
+})
+
+router.get('/:id', (req , res)  => {
+    req.render(`/articles/${article.id}`)
+})
+
+router.post('/' , async (req , res) => {
+    let article = new Article({
+        title : req.body.title,
+        description : req.body.description,
+        markdown : req.body.markdown,
+    })
+
+    try {
+        article = await article.save();
+        req.redirect(`/articles/${article.id}`)
+    }catch (e) {
+        res.render('/articles/new' , {article: article})
+    }
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express= require('express');
-
+const mongoose = require('mongoose')
 const app = express();
+
+mongoose.connect('mongodb://localhost/', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+app.use(express.urlencoded({extended:true}))
+
 const articleRouter = require('./routes/articles')
 app.set('view engine' , 'ejs')
-
 
 app.use('/articles' , articleRouter);
 app.get('/' , (req , res) => {
